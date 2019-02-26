35eefd82eb4812e06286882aa70d04b53a0eee69
Williams <Williams@wya.wil.i.am@gmail.com>
2019-02-26 03:00:50 -0800

    login page recreated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource_or_scope)
-    drone_drone_list_path
+    index_index_path
   end
 
   def after_sign_out_path_for(resource_or_scope)
