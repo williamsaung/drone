19156f487107417a7df9d3ca28685836cfb74988
Williams <Williams@wya.wil.i.am@gmail.com>
2019-03-07 09:18:39 -0800

    added security for RESTAPI

--- app/controllers/api/v1/missions_controller.rb
+++ app/controllers/api/v1/missions_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class MissionsController < ApplicationController
+      before_action :authorize_request, except: :create
+      before_action :find_user, except: %i[create index]
       protect_from_forgery with: :null_session
       respond_to :json
       def index
