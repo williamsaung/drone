82e232971aa9f141727eaebdd682b6ae5cd45e8c
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-03-07 20:38:55 +0700

    Added conn string and description to drone schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_01_035246) do
+ActiveRecord::Schema.define(version: 2019_03_07_132747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_03_01_035246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "connection_string"
+    t.string "description"
     t.index ["user_id"], name: "index_drones_on_user_id"
   end
 
