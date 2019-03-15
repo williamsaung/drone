862c17cbd21212dd05537bc367ec32f1896ea7ab
AdamHtooLwin <adamhtoolwin@gmail.com>
2019-03-15 18:38:59 +0700

    Re fixed script location for sim on ubuntu server

--- app/controllers/drone_controller.rb
+++ app/controllers/drone_controller.rb
@@ -92,8 +92,9 @@ class DroneController < ApplicationController
     connection_string = @drone.connection_string
     puts "Executing command 'python ~/drone-comms/drone/mission.py #{connection_string} #{gps_latitude} #{gps_longitude}'"
 
+    # For simulators
     if @drone.simulator?
-      child_pid = spawn({"PATH" => "/home/pi/.pyenv/shims:/home/pi/.pyenv/bin:/home/pi/.rbenv/plugins/ruby-build/bin:/home/pi/.rbenv/shims:/home/pi/.rbenv/bin:/home/pi/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/pi/.local/bin"}, "python ~/drone-comms/drone/mission.py #{connection_string} #{gps_latitude} #{gps_longitude} --drone_id #{@drone.id}")
+      child_pid = spawn({"PATH" => "/home/ubuntu/.pyenv/shims:/home/ubuntu/.pyenv/bin:/home/ubuntu/.rbenv/plugins/ruby-build/bin:/home/ubuntu/.rbenv/shims:/home/ubuntu/.rbenv/bin:/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.local/bin"}, "python ~/drone-comms/drone/mission.py #{connection_string} #{gps_latitude} #{gps_longitude} --drone_id #{@drone.id}")
       # child_pid = spawn({"PATH" => "/home/adam/.pyenv/shims:/home/adam/.pyenv/bin:/home/adam/.rbenv/plugins/ruby-build/bin:/home/adam/.rbenv/shims:/home/adam/.rbenv/bin:/home/adam/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/adam/.local/bin"}, "python ~/drone/drone-comms/drone/mission.py #{connection_string} #{gps_latitude} #{gps_longitude} --drone_id #{@drone.id}")
       Process.detach(child_pid)
     end
