6773fd2d9a2d21dc0bdef768fdd3c4e38027a94d
Williams <Williams@wya.wil.i.am@gmail.com>
2019-03-24 06:13:38 -0700

    added tracker link in mission list

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -3,6 +3,7 @@ class Mission < ApplicationRecord
   belongs_to :location
 
 
+
   def self.search(params)
     missions = Mission.where("Location LIKE ? or Name LIKE ?", "%{params[:search]}%", "%#{params[:search]}%") if params[:search].present?
     missions
