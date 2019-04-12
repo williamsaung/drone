3964664457e0263c6f1cd3c872ab3190af3bf168
Williams <Williams@wya.wil.i.am@gmail.com>
2019-04-11 23:11:45 -0700

    fix mission create bug

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -41,7 +41,7 @@ class MissionsController < ApplicationController
   def create
     @mission = Mission.new(mission_params.merge(user_id: current_user.id))
     @mission.status = "Saved"
-    @mission.type = "Delivery"
+    @mission.mission_type = "Delivery"
 
     respond_to do |format|
       if @mission.save
