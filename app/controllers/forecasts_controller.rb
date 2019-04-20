53eae87b495dd35de4fa9902a90d769f33c8fdbb
thwin <thwinemaung@gmail.com>
2019-04-20 21:18:16 +0700

    Add battery level

--- app/controllers/forecasts_controller.rb
+++ app/controllers/forecasts_controller.rb
@@ -6,6 +6,9 @@ class ForecastsController < ApplicationController
     @forecast.save
     @weather = @forecast.get_weather_data
     @current_weather = @weather.currently
+
+    @sim_battery = NavLog.where(:drone_id => 1).last.battery_level
+    @real_battery = NavLog.where(:drone_id => 2).last.battery_level
   end
 
   # def http
